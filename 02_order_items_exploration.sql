b474f0d5c87da7335080dcd96b7bab11a1b50af0
jerrami-online <johan.errami@gmail.com>
2026-06-01 07:25:14 -0400

    Update 02_order_items_exploration.sql

--- 02_order_items_exploration.sql
+++ 02_order_items_exploration.sql
@@ -1,3 +1,11 @@
+/*
+Purpose:
+Explore the order_items table.
+
+This table contains the individual products purchased
+within each order, along with the revenue generated.
+*/
+
 select * 
 from orders_items
 order by id
