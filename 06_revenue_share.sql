5c7dc442e055bae76b882a7eb8f4ec4370730309
jerrami-online <johan.errami@gmail.com>
2026-06-01 07:31:15 -0400

    Create 06_revenue_share.sql

---
+++ 06_revenue_share.sql
@@ -0,0 +1,19 @@
+/*
+Purpose:
+Measure the percentage contribution of each product
+to total company revenue.
+
+A subquery is used to calculate total revenue across
+all products.
+*/
+
+select name,
+       round(
+            sum(amount_paid) /
+            (select sum(amount_paid) from order_items)
+            * 100.0,
+            2
+       ) as pct
+from order_items
+group by 1
+order by 2 desc;
